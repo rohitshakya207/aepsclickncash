0860134cf815ef53ada0a374c822e0d67066481d
rohitshakya207 <rohitshakya207@gmail.com>
2022-04-13 15:12:18 +0530

    second commit

---
+++ clickncash_aeps_library/src/main/java/com/clickncash_aeps_library/TestMessageClick.java
@@ -0,0 +1,12 @@
+package com.clickncash_aeps_library;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class TestMessageClick {
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+}
